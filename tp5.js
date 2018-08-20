c1faf9024db1f1086df006b170326f0c2c20140b
youcef kolli <32937714+youcefRG@users.noreply.github.com>
2018-08-20 12:04:40 +0100

    Add files via upload

---
+++ tp5.js
@@ -0,0 +1,18 @@
+console.log("hzllo <woels")
+
+var youcef = document.getElementById('error1');
+youcef.style.display = 'none';
+function validation(x) {
+
+
+if (x.value.length >= 3) {
+youcef.style.display = 'none';
+return true;
+}
+else {}
+}
+
+var youcef = document.getElementById('error2');
+youcef.style.display = 'none';
+
+
